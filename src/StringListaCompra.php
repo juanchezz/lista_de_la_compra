04372d340e9f73d16b175efa6c7d151507f5065d
juanchezz <sanchez.138940@e.unavarra.es>
2025-04-01 13:25:48 +0200

    [refactor] Función añadir

--- src/StringListaCompra.php
+++ src/StringListaCompra.php
@@ -4,5 +4,23 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringListaCompra
 {
-    // TODO: String Calculator Kata
+    private array $productos = [];
+
+    public function ejecutar($string): string
+    {
+        if (str_starts_with(strtolower($string), 'añadir')) {
+            $partes = explode(' ', $string);
+            $nombre = strtolower($partes[1]);
+            $cantidad = $partes[2] ?? 1;
+            $this->productos[$nombre] = ($this->productos[$nombre] ?? 0) + (int)$cantidad;
+
+            ksort($this->productos, SORT_STRING | SORT_FLAG_CASE);
+
+            return implode(', ', array_map(
+                fn($nombre) => "$nombre x{$this->productos[$nombre]}",
+                array_keys($this->productos)
+            ));
+        }
+        return '';
+    }
 }
